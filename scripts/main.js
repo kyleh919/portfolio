979df6fbe8c9af97c991bcde71a8a2e1f06089c0
Kyle Hunter <kyle.hunter91@gmail.com>
2019-11-30 22:30:01 -0800

    Starts setting up the menu nav hidden by the hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 // query selectors
 let menuBtn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
+let menuNav = document.querySelector(".menu-nav");
+let navItems = document.querySelectorAll(".nav-item");
 
 // sets default state of menu
 let showMenu = false;
@@ -9,9 +12,17 @@ function toggleMenu() {
   console.log("toggled menu!");
   if (!showMenu) {
     menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(navItem => navItem.classList.add("show"));
+
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(navItem => navItem.classList.remove("show"));
+
     showMenu = false;
   }
 }
