17f695a65ea6b173364f148a8d04b8fbf018a346
Kyle Hunter <kyle.hunter91@gmail.com>
2019-11-30 21:25:16 -0800

    Adds js logic to add a class if the menu should be shown or not by clicking the hamburger menu button

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
+// query selectors
 let menuBtn = document.querySelector(".menu-btn");
 
+// sets default state of menu
+let showMenu = false;
+
+// toggles menu on user click
 function toggleMenu() {
   console.log("toggled menu!");
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    showMenu = false;
+  }
 }
 
 menuBtn.addEventListener("click", toggleMenu);
